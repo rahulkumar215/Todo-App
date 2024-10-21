c9f07e9d992f9de0b26c2f4ded4ccb2380056aca
rahulkumar215 <rk1617015@gmail.com>
2024-10-22 01:21:09 +0530

    added base styling

---
+++ comment.jsx
@@ -0,0 +1,12 @@
+// Todo
+
+// <!-- Add dynamic number -->
+// items left All Active Completed Clear Completed Drag and drop to reorder
+// list
+
+// <div class="attribution">
+//   Challenge by
+//   <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+//     >Frontend Mentor</a
+//   >. Coded by <a href="#">Your Name Here</a>.
+// </div>
